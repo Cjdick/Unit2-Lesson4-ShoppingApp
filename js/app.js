46d63a7adddb3945b30fdf45ea1f88a00ab7d5ba
Cjdick <chrisdueck87@gmail.com>
2014-07-09 14:53:44 -0700

    Reduced to functionality basic
    Discuss removing all selected items and UI popups

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.box').mousedown(function() {
+  $(document).on('click','.box', function() {
   	//alert("Clicked");
   	$(this).toggleClass('boxRemoved');
   	$(this).closest('.ListItem').find('.ItemText').toggleClass('ItemTextRemoved');
@@ -8,11 +8,45 @@ $(document).ready(function() {
    // 	alert("true");
     //}
     })
-   $(".ItemRemove, ItemRemoved").mousedown(function() {
+     $(document).on('click','.ItemRemove, ItemRemoved', function() {
   		$(this).closest('.ListItem').remove();
   		//Having difficulty selecting the intersection of ItemRemove and ItemRemoved
     })
     $("#add").click(function() {
   		$(this).closest('.main').find('.ListContainer').prepend("<div class='ListItem'><div class='box'></div><div class='ItemText'><p>"+$("#entry").val()+"</p></div><div class='ItemRemove'></div></div>");
     })
-  })
+    $(".RemoveAll").click(function () {
+  		$(this).closest('.main').find('.ListContainer').empty();
+    })
+})
+/*
+function fnOpenNormalDialog() {
+	//var $ =jQuery.noConflict();
+    $("#dialog-confirm").html("Confirm Dialog Box");
+    // Define the Dialog and its properties.
+    $("#dialog-confirm").dialog({
+        resizable: false,
+        modal: true,
+        title: "Modal",
+        height: 250,
+        width: 400,
+        buttons: {
+            "Yes": function () {
+                $(this).dialog('close');
+                callback(true);
+            },
+                "No": function () {
+                $(this).dialog('close');
+                callback(false);
+            }
+        }
+    })
+}
+
+function callback(value) {
+    if (value) {
+        alert("Confirmed");
+    } else {
+        alert("Rejected");
+    }
+}*/
